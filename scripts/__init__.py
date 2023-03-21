12e16a9ba74a4c242edfbf2af5c9e11fdd208c64
qinshize <qinshize@zingfront.com>
2023-03-21 10:39:55 +0800

    fix: adlib 参数更新增加日志

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time     : 2023/3/18 15:22
+# @Author   : qinshize@zingfront.com
+# @File     : __init__.py.py
+# @Software : SocialPeta
+# @description :
